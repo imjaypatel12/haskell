a8852cdd01b0b3f1a6ab6bc10b138047fdbc6dd2
Jay Patel <imjaypatel12@gmail.com>
2019-02-04 00:50:31 -0800

    final code; removed suboptimal impl

--- goldbach-conj-exceptions.py
+++ goldbach-conj-exceptions.py
@@ -2,85 +2,43 @@ from itertools import count
 from time import time
 import math
 
-# v1
 primes = [2, 3]
+def isPrime(n):
+    (limit, i) = (math.sqrt(n), 0)
+    while(getPrime(i) <= limit):
+        if n % getPrime(i) == 0: return False
+        i+=1
+    else:
+        return True
+
 def getPrime(index=0):
     if index < len(primes):
         return primes[index]
     else:
         for odd in count(getPrime(index-1) + 2,2):
-            flag = True
-            for p in range(index):
-                if odd % getPrime(p) == 0:
-                    flag = False
-                    break
-            if flag:
+            if isPrime(odd):
                 primes.append(odd)
-                break
-        return primes[index]
-
-def isPrime(n):
-    i = 0
-    while getPrime(i) <= math.sqrt(n):
-        if getPrime(i) == n: return True
-        i+=1
-    else:
-        return getPrime(i) == n
+                return primes[index]
 
 
-# v2
-primes2 = [2, 3]
-def getPrime2(index=0):
-    if index < len(primes2):
-        return primes2[index]
-    else:
-        for odd in count(getPrime2(index-1) + 2,2):
-            if isPrime2(odd):
-                primes2.append(odd)
-                break
-        return primes2[index]
-
-def isPrime2(n):
-    """Returns True if n is prime."""
-    if n in [2, 3]:
-        return True
-    if n % 2 == 0 or n % 3 ==0:
-        return False
-
-    i, w = 5, 2
-    
-    while i * i <= n:
-        if n % i == 0:
-            return False
-
-        i += w
-        w = 6 - w
-
-    return True
-
-# switch the impls
-is_prime = isPrime2
-get_prime = getPrime2
-
-def getCompositeOdd():
+def compositeOdds():
     for odd in count(3,2):
-        if not is_prime(odd):
+        if not isPrime(odd):
             yield odd
 
 def isASquare(n):
     return n**0.5 == int(n**0.5)
 
-def isValidComp(comp):
+def isExceptionalComp(comp):
     i = 0
-    while(comp > get_prime(i)):
-        if isASquare((comp - get_prime(i))/2): 
-            return False
+    while(comp > getPrime(i)):
+        if isASquare((comp - getPrime(i))/2): return False
         i+=1
     return True
 
 def getGoldbackConjException():
-    for comp in getCompositeOdd():
-        if isValidComp(comp):
+    for comp in compositeOdds():
+        if isExceptionalComp(comp):
             yield comp
 
 def getNGoldbackConjException(n):
