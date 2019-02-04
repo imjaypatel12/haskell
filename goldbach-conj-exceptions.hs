d350694e347b1c156393cbcd27218577fb1f83ba
Jay Patel <imjaypatel12@gmail.com>
2019-02-04 00:56:32 -0800

    fixed type def in hs

--- goldbach-conj-exceptions.hs
+++ goldbach-conj-exceptions.hs
@@ -5,7 +5,7 @@ import Math.NumberTheory.Powers.Squares
 
 {- Method A
    generates a list of squares -}
-squares2 :: [Integral]
+squares2 :: [Integer]
 squares2 = [d*d | d <- [1, 2 .. ]]
 
 {- dropWhile starts a list from the first element that is not less than the given
@@ -15,47 +15,47 @@ isASquare2 n = n == head (dropWhile (<n) squares2)
 
 {- Method B; Dr Abbott's method; best after the arithmoi lib's function
    A simple mathematical check for a perfect square -}
-iSqrt :: Integral -> Integral
+iSqrt :: Integer -> Integer
 iSqrt n = round (sqrt (fromIntegral n))
 
-isASquare :: Integral -> Bool
+isASquare :: Integer -> Bool
 isASquare n = (iSqrt n) ^ 2 == n
 -- 0.39
 
 {- Method C
    includes an initial screen i.e. prime always ends with [0,1,4,5,6,9] --}
-lastDigit :: Integral -> Integral
+lastDigit :: Integer -> Integer
 lastDigit num | num < 0 = ((-1) * num) `rem` 10 | otherwise = num `rem` 10
 
-isASquare3 :: Integral -> Bool
+isASquare3 :: Integer -> Bool
 isASquare3 n | lastDigit n `elem` [0, 1, 4, 5, 6, 9] = isASquare n | otherwise = False
 -- 0.61
 
-oddsFrom3 :: [Integral]
+oddsFrom3 :: [Integer]
 oddsFrom3 = [3, 5 .. ]
 
 {- generate prime numbers recursively; checks for all odd numbers that if there
    exists a number that is less than or equals to the sqrt of the current odd number.
    If yes, current odd number is prime. --}
-smallPrimeDivisors :: Integral -> [Integral]
+smallPrimeDivisors :: Integer -> [Integer]
 smallPrimeDivisors n = [d | d <- takeWhile (\x -> x^2 <= n) primes, n `mod` d == 0]
 
-primes :: [Integral]
+primes :: [Integer]
 primes = 2 : [n | n <- oddsFrom3, null (smallPrimeDivisors n)]
 
 {- evaluates the mod operation for a given input against all the previous primes
    this evaluation is limited till the sqrt(n) as, for any number N, there exists
    a factor f1 that is <= sqrt(N).
    This optimization grately improves the primality test -}
-isPrime :: Integral -> Boolean
+isPrime :: Integer -> Bool
 isPrime n = null [p | p <- takeWhile(<= iSqrt n) primes, n `mod` p == 0]
 
 -- generates a list of composite numbers. composites are the odd numbers (excuding 1) that are not prime.
-oddComposites :: [Integral]
+oddComposites :: [Integer]
 oddComposites = filter(\x-> not(isPrime x)) oddsFrom3
 
 {- Goldbach’s-other-conjecture problem; finds composites that does not follow:
    "Every odd composite number can be expressed as the sum of a prime and twice a square."
    Use different impl of isASquare to check various methods. Use isSquare for arithmoi lib -}
-goldbachExceptions :: [Integral]
+goldbachExceptions :: [Integer]
 goldbachExceptions = [g | g <- oddComposites, null [p | p <- takeWhile(<g) primes, isSquare ((g-p) `div` 2)] ]
--- goldbach-conj-exceptions.py
+++ goldbach-conj-exceptions.py
@@ -3,48 +3,58 @@ from time import time
 import math
 
 primes = [2, 3]
+
+
 def isPrime(n):
     (limit, i) = (math.sqrt(n), 0)
-    while(getPrime(i) <= limit):
-        if n % getPrime(i) == 0: return False
-        i+=1
+    while getPrime(i) <= limit:
+        if n % getPrime(i) == 0:
+            return False
+        i += 1
     else:
         return True
+
 
 def getPrime(index=0):
     if index < len(primes):
         return primes[index]
     else:
-        for odd in count(getPrime(index-1) + 2,2):
+        for odd in count(getPrime(index-1) + 2, 2):
             if isPrime(odd):
                 primes.append(odd)
                 return primes[index]
 
 
 def compositeOdds():
-    for odd in count(3,2):
+    for odd in count(3, 2):
         if not isPrime(odd):
             yield odd
+
 
 def isASquare(n):
     return n**0.5 == int(n**0.5)
 
+
 def isExceptionalComp(comp):
     i = 0
     while(comp > getPrime(i)):
-        if isASquare((comp - getPrime(i))/2): return False
-        i+=1
+        if isASquare((comp - getPrime(i))/2):
+            return False
+        i += 1
     return True
+
 
 def getGoldbackConjException():
     for comp in compositeOdds():
         if isExceptionalComp(comp):
             yield comp
 
+
 def getNGoldbackConjException(n):
     g = getGoldbackConjException()
     for i in range(n):
         print(next(g))
+
 
 start = time()
 getNGoldbackConjException(2)
